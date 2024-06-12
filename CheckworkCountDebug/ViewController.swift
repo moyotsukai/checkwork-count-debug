6d85662a66daaf8db21b304bc7efbad756c7bd4e
moyotsukai <gyoshu.bowie@gmail.com>
2024-06-13 00:26:57 +0900

    change: assignment

--- CheckworkCountDebug/ViewController.swift
+++ CheckworkCountDebug/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var number: Int = 0
+    //MARK: ⑤ 初期値を0〜100の間の乱数にしよう！（ヒント: 乱数はInt.random()を使うよ）
+    var number: Int = Int.random(in: 0...100)
     
     @IBOutlet var label: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = String(number)
     }
     
     // ------以下の①〜③のエラー修正に取り組んでみよう！------
@@ -26,17 +29,7 @@ class ViewController: UIViewController {
         //MARK: ② エラーを直そう！
         label.text = String(number)
         
-        if (number > 10) {
-            label.textColor = .red
-        } else {
-            label.textColor = .black
-        }
-    }
-    
-    @IBAction func minus() {
-        number = number - 1
-        label.text = String(number)
-        
+        //MARK: ④ numberが10を超えたら数字の色が赤に変わるようにしてみよう！
         if (number > 10) {
             label.textColor = .red
         } else {
@@ -45,10 +38,8 @@ class ViewController: UIViewController {
     }
     
     //MARK: ③ 上の①,②を直しただけだとまだ動かない！何でかな？（ヒント: Storyboardで作業が必要だよ！）
-
+    
     
     // ------全部できたらエラー修正は完璧！ 余裕があったら以下の発展問題もやってみよう！------
-    //MARK: ④ マイナスボタンもつけてみよう！
-    //MARK: ⑤ numberが10を超えたら数字の色が赤に変わるようにしてみよう！
     
 }
